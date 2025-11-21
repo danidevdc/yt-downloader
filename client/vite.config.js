55fba66a37e6eec54f19bf35af39d110b647fdd6
Dani <lp.developer.dc@gmail.com>
2025-11-21 12:10:51 -0400

    Trigger deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,4 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/yt-downloader/', // Relative base path for GitHub Pages
+  // Trigger deployment
 })
